592d5246cbef5dc0bd35222d169edb6a6fd34cbd
Mauricio Cartagena <mc973220@gmail.com>
2020-09-25 18:43:00 -0400

    se hizo el el map de ["Marvel comics","DC comics"']

--- src/routers/DashBoardRoutes.js
+++ src/routers/DashBoardRoutes.js
@@ -9,7 +9,7 @@ export const DashBoardRoutes = () => {
     return (
         <>
             <Navbar/>
-            <div>
+            <div className='container mt-2' >
                 <Switch>
                     <Route exact path="/marvel"  component={ MarvelScreen } />
                     <Route exact path="/heroe/:heroeId"  component={ HeroScreen } />
